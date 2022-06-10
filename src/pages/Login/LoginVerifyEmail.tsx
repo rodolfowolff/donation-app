f7ae3c0a9f2a32932758302a193705f03e249605
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-06-10 18:56:45 -0300

    fix: placeholder by type user

--- src/pages/Login/LoginVerifyEmail.tsx
+++ src/pages/Login/LoginVerifyEmail.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { StatusBar, View, Pressable } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { useTheme } from 'styled-components/native';
+import React from "react";
+import { StatusBar, View, Pressable } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useTheme } from "styled-components/native";
 
-import { Button, Typography, Input  } from '../../components/common';
-import { Container, Content, BgImage } from '../../styles/global.style';
-import imageOnboard from '../../assets/images/onboarding-bg.png';
-import Icon from '@expo/vector-icons/FontAwesome5';
+import { Button, Typography, Input } from "../../components/common";
+import { Container, Content, BgImage } from "../../styles/global.style";
+import imageOnboard from "../../assets/images/onboarding-bg.png";
+import Icon from "@expo/vector-icons/FontAwesome5";
 
 const LoginVerifyEmail = () => {
   const { colors } = useTheme();
@@ -14,42 +14,53 @@ const LoginVerifyEmail = () => {
   const { params }: any = useRoute();
 
   return (
-    <Container style={{ backgroundColor: colors.primary}}>
-      <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+    <Container style={{ backgroundColor: colors.primary }}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
       <BgImage source={imageOnboard} resizeMode="cover" />
       <Content withHeader>
-
         <Pressable onPress={() => goBack()}>
           <Icon name="chevron-left" size={24} color="white" />
         </Pressable>
 
-        <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom:20 }}>
-          <Typography color='white' size='xxlarge' weight='bold'>
+        <View style={{ flex: 1, justifyContent: "flex-end", marginBottom: 20 }}>
+          <Typography color="white" size="xxlarge" weight="bold">
             {`Sua doação ajudará\nmuitas pessoas`}
           </Typography>
 
-          <Typography color='white' size='medium' weight='regular' style={{ marginTop:10, marginBottom:15 }}>
-            { params.type === 'donation' ? 
-              'Informe seu email que iremos verificar se você já possui cadastro' : 
-              'Informe o cnpj da sua ONG que iremos verificar se já possui cadastro' 
-            }
+          <Typography
+            color="white"
+            size="medium"
+            weight="regular"
+            style={{ marginTop: 10, marginBottom: 15 }}
+          >
+            {params.type === "donation"
+              ? "Informe seu email que iremos verificar se você já possui cadastro"
+              : "Informe o cnpj da sua ONG que iremos verificar se já possui cadastro"}
           </Typography>
 
-          <Input placeholder='Informe seu email' />
+          <Input
+            placeholder={`Informe ${
+              params.type === "donation" ? "seu CPF" : "o CNPJ"
+            }`}
+          />
         </View>
 
-        <Button 
-          title='Continuar' 
-          bgColor='white' 
-          txtColor='primary' 
-          size="large"  
-          weight='bold' 
+        <Button
+          title="Continuar"
+          bgColor="white"
+          txtColor="primary"
+          size="large"
+          weight="bold"
           margin={30}
-          onPress={() => navigate('LoginPassword', { type: params.type })}
+          onPress={() => navigate("LoginPassword", { type: params.type })}
         />
       </Content>
     </Container>
   );
-}
+};
 
 export default LoginVerifyEmail;
--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,48 +1,57 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { useTheme } from 'styled-components/native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
 
-import { Button, Typography  } from '../../components/common';
-import { Container, Content, BgImage } from '../../styles/global.style';
-import imageOnboard from '../../assets/images/onboarding-bg.png';
+import { Button, Typography } from "../../components/common";
+import { Container, Content, BgImage } from "../../styles/global.style";
+import imageOnboard from "../../assets/images/onboarding-bg.png";
 
 const Onboarding = () => {
   const { colors } = useTheme();
   const { navigate } = useNavigation();
 
   return (
-    <Container style={{ backgroundColor: colors.primary}}>
-      <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+    <Container style={{ backgroundColor: colors.primary }}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
       <BgImage source={imageOnboard} resizeMode="cover" />
-      <Content style={{flex: 1, justifyContent: 'flex-end'}}>
-        <Typography color='white' size='xxlarge' weight='bold'>
+      <Content style={{ flex: 1, justifyContent: "flex-end" }}>
+        <Typography color="white" size="xxlarge" weight="bold">
           {`Todos nós podemos\nser solidários`}
         </Typography>
-        <Typography color='white' size='medium' weight='regular' style={{marginTop:10, marginBottom:15}}>
+        <Typography
+          color="white"
+          size="medium"
+          weight="regular"
+          style={{ marginTop: 10, marginBottom: 15 }}
+        >
           Selecione abaixo se você deseja entrar como doador ou como ONG
         </Typography>
-        <Button 
-          title='Entrar como ONG'
-          bgColor='white'
-          txtColor='white'
+        <Button
+          title="Entrar como ONG"
+          bgColor="white"
+          txtColor="white"
           size="large"
-          weight='bold'
-          outline 
+          weight="bold"
+          outline
           onPress={() => navigate("LoginVerifyEmail", { type: "ong" })}
         />
-        <Button 
-          title='Entrar como Doador'
-          bgColor='white'
-          txtColor='primary'
+        <Button
+          title="Entrar como Doador"
+          bgColor="white"
+          txtColor="primary"
           size="large"
-          weight='bold'
+          weight="bold"
           margin={20}
           onPress={() => navigate("LoginVerifyEmail", { type: "donation" })}
         />
       </Content>
     </Container>
   );
-}
+};
 
 export default Onboarding;
